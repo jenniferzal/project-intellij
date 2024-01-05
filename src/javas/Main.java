b5f27d93474aafebde73b455d5ca305e3ca4e354
aweso <jennizaru11@gmail.com>
2024-01-05 14:09:10 -0800

    Refactored my Main class.

--- src/javas/Main.java
+++ src/javas/Main.java
@@ -3,7 +3,17 @@ package javas;
 public class Main
 {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
-        System.out.println("By: Jennifer");
+        printMessage("Hello, world!");
+        printMessage("By: Jennifer");
+    }
+
+    private static void printMessage(String message)
+    {
+        System.out.println(message);
+    }
+
+    private static void printHello()
+    {
+        System.out.println("Hello");
     }
 }

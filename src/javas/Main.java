371433baf857aba6111a7a1b24d1c8487e5c80bd
aweso <jennizaru11@gmail.com>
2024-01-05 14:19:31 -0800

    Added a method to print a  header.

--- src/javas/Main.java
+++ src/javas/Main.java
@@ -12,4 +12,8 @@ public class Main
         System.out.println(message);
     }
 
+    private static void printHeader()
+    {
+        System.out.println("**********************************");
+    }
 }

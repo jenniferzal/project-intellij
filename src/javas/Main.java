a18801ebf9d9ab130f3c6e9dfe60ab29ff79ec90
aweso <jennizaru11@gmail.com>
2024-01-05 14:24:33 -0800

    Revert "Refactored my Main class."
    This reverts commit b5f27d93

--- src/javas/Main.java
+++ src/javas/Main.java
@@ -3,17 +3,7 @@ package javas;
 public class Main
 {
     public static void main(String[] args) {
-        printMessage("Hello, world!");
-        printMessage("By: Jennifer");
-    }
-
-    private static void printMessage(String message)
-    {
-        System.out.println(message);
-    }
-
-    private static void printHeader()
-    {
-        System.out.println("**********************************");
+        System.out.println("Hello, world!");
+        System.out.println("By: Jennifer");
     }
 }
